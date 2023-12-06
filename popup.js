0a1cc0ff0136220f9af0d23d8b11bd9f2d049ff8
João Santos <32807182+jvpdls@users.noreply.github.com>
2023-12-06 19:04:37 -0300

    added docs

--- popup.js
+++ popup.js
@@ -1,4 +1,11 @@
 /**
+ * @fileoverview This file contains the logic for the popup window.
+ * @version 1.0.0
+ * @since 1.0.0
+ * @license MIT License. See LICENSE.md for more details.
+ */
+
+/**
  * Adds a click event listener to the 'convert' element and sends a message to the active tab to perform a conversion.
  * @param {Event} event - The click event object.
  * @returns {void}
